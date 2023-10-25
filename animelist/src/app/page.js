6082f5756e99f3fee7abb38ca27bdb489f959a76
Andry Ariadi <andryariadi23@gmail.com>
2023-10-25 21:23:41 +0700

    add btn see all

--- animelist/src/app/page.js
+++ animelist/src/app/page.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Card from "./components/Card";
 
 export default async function Home() {
@@ -7,7 +8,12 @@ export default async function Home() {
 
   return (
     <>
-      <h1>Welcome Animelist</h1>
+      <div className="flex justify-between py-5 px-[150px]">
+        <h1 className="text-2xl font-bold">Welcome Animelist</h1>
+        <Link href="/populer" className="bg-white border-yellow-600 border-2 hover:bg-yellow-500 transition-all duration-500 hover:text-white font-medium text-yellow-600 py-1 px-3 rounded-lg">
+          See All
+        </Link>
+      </div>
       <div className="container mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-3 w-[100%] lg:w-[80%] mx-auto px-10 md:px-0 lg:px-0">
           {anime.data.map((data) => {
