52ee60d5b6dc9966a5e6d8d50282191c993a220a
Andry Ariadi <andryariadi23@gmail.com>
2023-10-26 13:48:50 +0700

    add header component

--- animelist/src/app/page.js
+++ animelist/src/app/page.js
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import Header from "@/components/Header";
 import Card from "../components/Card";
 
 export default async function Home() {
@@ -8,13 +8,14 @@ export default async function Home() {
 
   return (
     <>
-      <div className="flex justify-between items-center py-5 px-3 md:px-10 lg:px-[150px]">
-        <h1 className="text-2xl font-bold">Top Anime</h1>
-        <Link href="/populer" className="bg-white border-yellow-600 border-2 hover:bg-yellow-500 transition-all duration-500 hover:text-white font-medium text-yellow-600 py-1 px-3 rounded-lg">
-          See All
-        </Link>
-      </div>
-      <Card dataAnime={topAnime} key={topAnime.mal_id} />
+      <section>
+        <Header title="Top Anime" linkHref="/populer" linkTitle="See All" />
+        <Card dataAnime={topAnime} key={topAnime.mal_id} />
+      </section>
+      <section>
+        <Header title="Recomendation" linkHref="/recomendation" linkTitle="" />
+        <Card dataAnime={topAnime} key={topAnime.mal_id} />
+      </section>
     </>
   );
 }
---
+++ animelist/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function Header({ title, linkHref, linkTitle }) {
+  return (
+    <>
+      <div className="flex justify-between items-center py-5 px-3 md:px-10 lg:px-[150px]">
+        <h1 className="text-2xl font-bold">{title}</h1>
+        {linkTitle ? (
+          <Link href={linkHref} className="bg-white border-yellow-600 border-2 hover:bg-yellow-500 transition-all duration-500 hover:text-white font-medium text-yellow-600 py-1 px-3 rounded-lg">
+            {linkTitle}
+          </Link>
+        ) : null}
+      </div>
+    </>
+  );
+}
