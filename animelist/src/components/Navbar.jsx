0e0af34c3400f1ec4e93bade8c1c77d31ee32d4a
Andry Ariadi <andryariadi23@gmail.com>
2023-10-29 16:05:07 +0700

    feat: search

--- animelist/src/components/Navbar.jsx
+++ animelist/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import Icon from "@/app/assets/icon-navbar.svg";
+import { BiSearchAlt } from "react-icons/bi";
 
 export default function Navbar() {
   return (
@@ -11,7 +12,12 @@ export default function Navbar() {
             <Link href="/" className="font-bold text-2xl text-white">
               <Image src={Icon} width={150} height={150} />
             </Link>
-            <input type="text" placeholder="search..." className="rounded-lg px-3 focus:border-accent outline-none" />
+            <div className="flex relative">
+              <input type="text" placeholder="search..." className="rounded-lg px-3 focus:border-accent outline-none" />
+              <button className="absolute end-[5px] top-[5px] text-slate-600">
+                <BiSearchAlt size={25} />
+              </button>
+            </div>
           </div>
         </div>
       </header>
