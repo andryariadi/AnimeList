bc4f778180c5f767797df177a426b6df647326b2
Andry Ariadi <andryariadi23@gmail.com>
2023-10-31 23:10:11 +0700

    fetchdata all topanime

--- animelist/src/app/populer/page.jsx
+++ animelist/src/app/populer/page.jsx
@@ -1,6 +1,22 @@
+"use client";
+
 import Banner from "@/components/Banner";
+import { useEffect, useState } from "react";
 
 export default function Populer() {
+  const [page, setPage] = useState(1);
+  const [topAnime, setTopAnime] = useState([]);
+
+  const fetchData = async () => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?page=${page}`);
+    const data = await response.json();
+    setTopAnime(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [page]);
+
   return (
     <>
       <Banner />
