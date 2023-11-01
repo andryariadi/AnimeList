7564e8e5e3d4b6e684fd29d1a59e5d8355e34566
Andry Ariadi <andryariadi23@gmail.com>
2023-11-01 21:22:37 +0700

    add api resuable

--- animelist/src/components/Pagination.jsx
+++ animelist/src/components/Pagination.jsx
@@ -9,32 +9,41 @@ export default function Pagination({ number, setPage, lastPage }) {
   };
 
   const handleNextPage = () => {
-    setPage((next) => next + 1);
-    scroolTop();
+    if (number <= lastPage) {
+      setPage((next) => next + 1);
+      scroolTop();
+    }
   };
 
   const handlePrevPage = () => {
-    setPage((prev) => prev - 1);
-    scroolTop();
+    if (number > 1) {
+      setPage((prev) => prev - 1);
+      scroolTop();
+    }
   };
+
   return (
     <>
       <div className="flex justify-center items-center gap-x-5 py-5 text-slate-200 hover:text-amber-500 transition-all duration-500 w-fit mx-auto">
-        <div onClick={handlePrevPage} className="flex items-center gap-x-1">
-          <button>
-            <TbPlayerTrackPrevFilled size={24} />
-          </button>
-          <p className="cursor-pointer">Prev</p>
-        </div>
+        {number > 1 && (
+          <div onClick={handlePrevPage} className="flex items-center gap-x-1">
+            <button>
+              <TbPlayerTrackPrevFilled size={24} />
+            </button>
+            <p className="cursor-pointer">Prev</p>
+          </div>
+        )}
         <p>
           {number} of {lastPage}
         </p>
-        <div onClick={handleNextPage} className="flex items-center gap-x-1">
-          <p className="cursor-pointer">Next</p>
-          <button>
-            <TbPlayerTrackNextFilled size={24} />
-          </button>
-        </div>
+        {number >= lastPage ? null : (
+          <div onClick={handleNextPage} className="flex items-center gap-x-1">
+            <p className="cursor-pointer">Next</p>
+            <button>
+              <TbPlayerTrackNextFilled size={24} />
+            </button>
+          </div>
+        )}
       </div>
     </>
   );
