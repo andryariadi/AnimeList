9df7bf9ab2ad23efe1c118ffb66eee5164da7326
Andry Ariadi <andryariadi23@gmail.com>
2023-11-01 22:19:08 +0700

    fetch detail anime

--- animelist/src/app/detail-anime/[id]/page.jsx
+++ animelist/src/app/detail-anime/[id]/page.jsx
@@ -1,8 +1,16 @@
-export default function DetailAnime({ params }) {
+import { getAnimeResponse } from "@/app/libs/api-libs";
+
+export default async function DetailAnime({ params }) {
   const { id } = params;
+
+  const anime = await getAnimeResponse(`anime/${id}`);
+  const dataAnime = anime.data;
+
   return (
     <>
-      <h1>{id} detail</h1>
+      <div className="py-5 px-3 md:px-10 lg:px-[150px]">
+        <h1 className="text-2xl font-bold text-zinc-300">{dataAnime.title}</h1>
+      </div>
     </>
   );
 }
