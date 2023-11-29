2f32cd19c767695a9833a86b2b98c3196a63b102
Andry Ariadi <andryariadi23@gmail.com>
2023-11-30 05:50:37 +0700

    add auth credentials

--- animelist/src/app/api/auth/[...nextauth]/route.js
+++ animelist/src/app/api/auth/[...nextauth]/route.js
@@ -1,14 +1,58 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOption = {
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "your name" },
+        email: { label: "Email", type: "email", placeholder: "youremail@example.com" },
+        password: { label: "Password", type: "password", placeholder: "********" },
+      },
+      async authorize(credentials) {
+        if (!credentials.username || !credentials.email || !credentials.password) {
+          throw new Error("All fields are required");
+        }
+
+        const user = {
+          id: 1,
+          name: credentials.username,
+          email: credentials.email,
+          password: credentials.password,
+        };
+
+        if (user) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user.id = token.id;
+      }
+      return session;
+    },
+  },
 };
 
 const authHandler = NextAuth(authOption);
--- animelist/src/app/profile/page.jsx
+++ animelist/src/app/profile/page.jsx
@@ -1,5 +1,6 @@
 import { authUserSession } from "@/libs/auth-libs";
 import Image from "next/image";
+import { FaUserCircle } from "react-icons/fa";
 
 export default async function Profile() {
   const user = await authUserSession();
@@ -8,9 +9,7 @@ export default async function Profile() {
     <>
       <div className="flex items-center justify-center h-screen gap-x-3">
         <div className="border border-amber-500 rounded-lg py-10 px-28 flex items-center gap-x-3">
-          <div>
-            <Image className="rounded-full" src={user.image} alt="Anime" width={200} height={200} />
-          </div>
+          <div>{user?.image ? <Image className="rounded-full" src={user.image} alt="Profile" width={200} height={200} /> : <FaUserCircle size={200} color="#fff" />}</div>
           <div className="flex flex-col gap-y-5">
             <h4 className="text-xl font-bold text-zinc-300">{user.name}</h4>
             <h4 className="text-xl font-bold text-zinc-300">{user.email}</h4>
