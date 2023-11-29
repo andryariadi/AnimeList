e9ab20034b6660716416ca1f4925ea4899a4a1ad
Andry Ariadi <andryariadi23@gmail.com>
2023-11-30 06:08:42 +0700

    add auth google

--- animelist/src/app/api/auth/[...nextauth]/route.js
+++ animelist/src/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOption = {
@@ -11,6 +12,10 @@ export const authOption = {
     GitHubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
     }),
     CredentialsProvider({
       name: "Credentials",
